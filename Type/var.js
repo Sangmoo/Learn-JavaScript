b4d7e51213d5fbcda76b30429762123a5c5e3dd7
Sangmoo <first_1st@naver.com>
2018-05-15 00:48:06 +0900

    Var

---
+++ Type/var.js
@@ -0,0 +1,10 @@
+let currentTempC = 22; // 한번만 선언 가능
+let val; // undefined
+
+const ROOM_NUMBER = 1; // 더이상 변경할 수 없는 고정값
+
+let Url = "https://github.com/Sangmoo/Learn-JavaScript"; // 식별자 = "리터럴"
+
+
+let str = "https://github.com/Sangmoo";
+str = "Url"; // 새로운 값 할당
